088a10512490e7d5f17e1e5006145f634f2ec6f1
Jml <justmemoryl@foxmail.com>
2018-08-26 18:44:27 +0800

    init project.

---
+++ src/main/java/cn/jml/pokonyan/user/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package cn.jml.pokonyan.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
